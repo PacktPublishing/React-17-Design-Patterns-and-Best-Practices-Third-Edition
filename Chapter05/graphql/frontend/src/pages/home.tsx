e7bdf4e500c2b735aef0421a42834447bc9600fe
FoggDev <foggdev@gmail.com>
2021-05-06 01:12:01 -0700

    Fixing Chapter05 - Graphql

--- Chapter05/graphql/frontend/src/pages/home.tsx
+++ Chapter05/graphql/frontend/src/pages/home.tsx
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom'
-
 const Page = () => (
   <div className="home">
     <h1>Home</h1>
 
     <ul>
-      <li><Link to="/dashboard">Go to Dashboard</Link></li>
+      <li><a href="/dashboard">Go to Dashboard</a></li>
     </ul>
   </div>
 )
